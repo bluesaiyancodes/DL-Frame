ea751206796a49677544f91cd6ffb68daa272856
bluesaiyancodes <blue.bishal@rocketmail.com>
2025-01-07 16:53:53 +0900

    update 01-07

--- main.py
+++ main.py
@@ -7,18 +7,18 @@ import src.utils.conutils as utils
 
 
 @hydra.main(version_base=None, config_path="configs", config_name="config")
-def main(cfg: DictConfig):
+def main(config: DictConfig):
     # check log printing
-    if cfg.other.log_print:
+    if config.other.log_print:
         console_handler = utils.log_printer()
         logging.getLogger().addHandler(console_handler)
 
     # Obtain a module-level logger
     logger = logging.getLogger(__name__)
 
-    print(f"Selected Dataset - {cfg.dataset.name}, see the config/log file for more details.")
+    print(f"Selected Dataset - {config.dataset.name}, see the config/log file for more details.")
     # log the configuration
-    logger.info("Experiment Configuration:\n%s", OmegaConf.to_yaml(cfg))  
+    logger.info("Experiment Configuration:\n%s", OmegaConf.to_yaml(config))  
     hydra_cfg = hydra.core.hydra_config.HydraConfig.get()
     out_dir = hydra_cfg['runtime']['output_dir']
     print(f"Experiment output directory: {out_dir}")
